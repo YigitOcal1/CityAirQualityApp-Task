30cf2c179b9457eb54e1e45d6ad68e02d1eefe28
laasque <ygthnl@gmail.com>
2022-08-24 05:30:49 +0300

    Project files are structured and DI is implemented.

---
+++ app/src/main/java/com/example/cityairqualityapp/CityAirQualityApplication.kt
@@ -0,0 +1,8 @@
+package com.example.cityairqualityapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CityAirQualityApplication:Application() {
+}
